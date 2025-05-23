73c3ab8382684042b082013a72997f0064784ad3
as12231 <sai.komma0808@gmail.com>
2025-05-24 00:09:46 +0530

    backend is connected to database(mongoose) and login and sigup api need to be check

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const app = express();
-const PORT = 5000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
 
-// Middleware to parse JSON
+dotenv.config();
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-// Test route
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected successfully"))
+  .catch((err) => console.log("❌ MongoDB connection error:", err));
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.use('/auth', AuthRouter);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running on 5000");
 });
