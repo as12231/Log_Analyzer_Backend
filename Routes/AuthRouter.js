feb4df365d48ea75b0706cc6ceb7e995048b37f6
as12231 <sai.komma0808@gmail.com>
2025-05-24 00:30:49 +0530

    signup api is working ::but login is not

--- Routes/AuthRouter.js
+++ Routes/AuthRouter.js
@@ -1,7 +1,11 @@
+// routes/auth.js or similar
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../Controllers/AuthController');
 
+router.get('/h', (req, res) => {
+    res.send("Hello, this is the API");
+});
 router.post('/signup', signup);
 router.post('/login', login);
 
